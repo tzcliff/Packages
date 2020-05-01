a3370ae9a1c410a1d93310a3ee294126e79ba252
Cliff <tzcliff@outlook.com>
2020-04-30 23:25:16 -0400

    First commit

---
+++ Net/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly:XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Fotos.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage mainPage = new MainPage();
+        }
+    }
+}
